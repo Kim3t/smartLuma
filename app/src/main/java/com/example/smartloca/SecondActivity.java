d602122c3454a78af38741ac8349025d1d73b054
Kim3t <Kim3tgame@gmail.com>
2025-11-09 02:59:15 +0100

    feat: adding a Button to go back to the Main Activity from Second

--- app/src/main/java/com/example/smartloca/SecondActivity.java
+++ app/src/main/java/com/example/smartloca/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.smartloca;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -14,6 +17,15 @@ public class SecondActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textSecond);
         textView.setText("Das ist die zweite Activity");
 
+        Button buttonOpenFirst = findViewById(R.id.buttonOpenFirst);
+        buttonOpenFirst.setOnClickListener(v -> {
+            // starten einer neuer Activity (in Context this starte second)
+            Intent intent = new Intent( SecondActivity.this, MainActivity.class);
+
+            // Übergibt an OS und holt aus Manifest die zweite Activity
+            // -> laden von neuer Activity
+            startActivity(intent);
+        });
     }
 
 }
